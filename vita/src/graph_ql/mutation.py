08b9e2b8dd9af30e5df9638993984a1e2b42c3bc
applyuser160 <syunsuke130121@gamil.com>
2024-12-22 17:24:23 +0900

    mutationに反映

--- vita/src/graph_ql/mutation.py
+++ vita/src/graph_ql/mutation.py
@@ -10,29 +10,36 @@ from model.graphql_type import (
     SubAccountGraphqlType,
 )
 from service.create_account_service import CreateAccountService
+from service.update_account_service import UpdateAccountService
+from service.delete_account_service import DeleteAccountService
 from util.err import VitaError
+from util.sql_model import SQLSession
+from util.logg import Logg
 
 
 @strawberry.type
 class Mutation:
 
+    def get_session(self):
+        return SQLSession(Logg())
+
     @strawberry.mutation
     def create_account(
         self, input: AccountGraphqlInput
     ) -> AccountGraphqlType | VitaError:
-        return CreateAccountService().execute(input)
+        return CreateAccountService(self.get_session()).execute(input)
 
     @strawberry.mutation
     def update_account(
         self, input: AccountGraphqlInput
     ) -> AccountGraphqlType | VitaError:
-        pass
+        return UpdateAccountService(self.get_session()).execute(input)
 
     @strawberry.mutation
     def delete_account(
         self, input: AccountGraphqlInput
     ) -> AccountGraphqlType | VitaError:
-        pass
+        return DeleteAccountService(self.get_session()).execute(input)
 
     @strawberry.mutation
     def create_sub_account(
