09a9af2329f5ebcec31eddc22ac8f97c084657fb
applyuser160 <syunsuke130121@gamil.com>
2024-12-30 18:28:56 +0900

    mutation反映

--- vita/src/graph_ql/mutation.py
+++ vita/src/graph_ql/mutation.py
@@ -12,6 +12,12 @@ from model.graphql_type import (
 from vita.src.service.create_account_service import CreateAccountService
 from vita.src.service.update_account_service import UpdateAccountService
 from vita.src.service.delete_account_service import DeleteAccountService
+from vita.src.service.create_sub_account_service import CreateSubAccountService
+from vita.src.service.update_sub_account_service import UpdateSubAccountService
+from vita.src.service.delete_sub_account_service import DeleteSubAccountService
+from vita.src.service.create_journal_entry_serivce import CreateJournalEntryService
+from vita.src.service.update_journal_entry_serivce import UpdateJournalEntryService
+from vita.src.service.delete_journal_entry_serivce import DeleteJournalEntryService
 from vita.src.util.err import VitaError
 from vita.src.util.sql_model import SQLSession
 from vita.src.util.logg import Logg
@@ -45,34 +51,34 @@ class Mutation:
     def create_sub_account(
         self, input: SubAccountGraphqlInput
     ) -> SubAccountGraphqlType | VitaError:
-        pass
+        return CreateSubAccountService(self.get_session()).execute(input)
 
     @strawberry.mutation
     def update_sub_account(
         self, input: SubAccountGraphqlInput
     ) -> SubAccountGraphqlType | VitaError:
-        pass
+        return UpdateSubAccountService(self.get_session()).execute(input)
 
     @strawberry.mutation
     def delete_sub_account(
         self, input: SubAccountGraphqlInput
     ) -> SubAccountGraphqlType | VitaError:
-        pass
+        return DeleteSubAccountService(self.get_session()).execute(input)
 
     @strawberry.mutation
     def create_journal_entry(
         self, input: JournalEntryGraphqlInput
     ) -> JournalEntryGraphqlType | VitaError:
-        pass
+        return CreateJournalEntryService(self.get_session()).execute(input)
 
     @strawberry.mutation
     def update_journal_entry(
         self, input: JournalEntryGraphqlInput
     ) -> JournalEntryGraphqlType | VitaError:
-        pass
+        return UpdateJournalEntryService(self.get_session()).execute(input)
 
     @strawberry.mutation
     def delete_journal_entry(
         self, input: JournalEntryGraphqlInput
     ) -> JournalEntryGraphqlType | VitaError:
-        pass
+        return DeleteJournalEntryService(self.get_session()).execute(input)
