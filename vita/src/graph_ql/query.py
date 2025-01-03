3ff16bdebddd38358ee489051a16ca53d36f833f
applyuser160 <syunsuke130121@gamil.com>
2025-01-03 18:20:09 +0900

    query追加

--- vita/src/graph_ql/query.py
+++ vita/src/graph_ql/query.py
@@ -12,6 +12,10 @@ from model.graphql_type import (
 )
 from vita.src.service.get_account_service import GetAccountService
 from vita.src.service.get_accounts_service import GetAccountsService
+from vita.src.service.get_journal_entries_service import GetJournalEntriesService
+from vita.src.service.get_journal_entry_service import GetJournalEntryService
+from vita.src.service.get_sub_account_service import GetSubAccountService
+from vita.src.service.get_sub_accounts_service import GetSubAccountsService
 from vita.src.util.err import VitaError
 from vita.src.util.logg import Logg
 from vita.src.util.sql_model import SQLSession
@@ -37,22 +41,22 @@ class Query:
     def sub_account(
         self, input: SingleGraphqlInput
     ) -> SubAccountGraphqlType | VitaError:
-        pass
+        return GetSubAccountService(self.get_session()).execute(input)
 
     @strawberry.field
     def sub_accounts(
         self, input: SubAccountsGraphqlInput
     ) -> list[SubAccountGraphqlType] | VitaError:
-        return []
+        return GetSubAccountsService(self.get_session()).execute(input)
 
     @strawberry.field
     def journal_entry(
         self, input: SingleGraphqlInput
     ) -> JournalEntryGraphqlType | VitaError:
-        pass
+        return GetJournalEntryService(self.get_session()).execute(input)
 
     @strawberry.field
     def journal_entries(
         self, input: JournalEntriesGraphqlInput
     ) -> list[JournalEntryGraphqlType] | VitaError:
-        return []
+        return GetJournalEntriesService(self.get_session()).execute(input)
